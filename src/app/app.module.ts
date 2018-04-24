dd6bba1dae31ec778acc3fce1845178e0a467fb2
Sarik <sareek007@gmail.com>
2018-04-24 17:24:15 +0545

    routes setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+const Routes = [
+   {
+     path: '',
+     redirectTo:'posts',
+     pathMatch:'full'
+
+   },
+   {
+     path: 'posts', component:PostsComponent
+   }
+
+
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(Routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
